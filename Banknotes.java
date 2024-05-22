caeadc3387d2f9c1b179273a348e7724887904f5
Shininden <mathpaz.oliveira@gmail.com>
2024-05-22 10:44:37 -0300

    Update Banknotes.java

--- Banknotes.java
+++ Banknotes.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class Banknotes 
 {
-    static int digit = 0;
+    static double digit = 0;
 
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
         
         System.out.print("How much money do you want to withdraw? ");
-        int requestedValue = sc.nextInt();
+        double requestedValue = sc.nextInt();
 
         int power = 0;
-        int real_1 = 0, real_2 = 0, real_5 = 0, real_10 = 0, real_20 = 0, real_50 = 0, real_100 = 0;
+        int real_2 = 0, real_5 = 0, real_10 = 0, real_20 = 0, real_50 = 0, real_100 = 0;
+        double cent_1 = 0, cent_050 = 0, cent_025 = 0, cent_010 = 0, cent_005 = 0, cent_001 = 0; 
 
         while(requestedValue > 0)
         {
@@ -72,7 +73,7 @@ public class Banknotes
 
                while ((digit - 1) >= 0)
                {
-                  real_1++;
+                  cent_1++;
                   digit -= 1;
                }
 
@@ -88,7 +89,7 @@ public class Banknotes
         System.out.println(real_10 + " nota(s) de R$ 10,00");
         System.out.println(real_5 + " nota(s) de R$ 5,00");
         System.out.println(real_2 + " nota(s) de R$ 2,00");
-        System.out.println(real_1 + " nota(s) de R$ 1,00");
+        System.out.println(cent_1 + " nota(s) de R$ 1,00");
 
         sc.close();
     }
