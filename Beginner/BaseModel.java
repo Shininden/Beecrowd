e1228bf5e3975e688a8cdf327af0d69ae357a892
Matheus Paz <mathpaz.oliveira@gmail.com>
2025-01-15 17:29:10 -0300

    Update BaseModel.java

--- Beginner/BaseModel.java
+++ Beginner/BaseModel.java
@@ -7,8 +7,11 @@ public class BaseModel
     public static void main(String[] args) 
     {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        //int n = sc.nextInt();
+
+
 
         sc.close();
     }
 }
+
